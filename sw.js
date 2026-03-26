4722497ccc210ac57d9cf7356acd0f0f9cb89f14
Opie <76084564+opiedopy@users.noreply.github.com>
2026-03-26 10:01:07 -0400

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,20 @@
+const cacheName = 'golf-sim-v1';
+const assets = ['./', './index.html', './manifest.json'];
+
+// Install the service worker and cache files
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Serve files from cache when offline
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
