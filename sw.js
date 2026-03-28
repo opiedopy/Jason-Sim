074340416ac6c378c16ba937bf47a717a398ad49
Opie <76084564+opiedopy@users.noreply.github.com>
2026-03-27 21:12:41 -0400

    Update service worker cache version and assets
    Updated cache name to indicate versioning and modified asset list.

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-const cacheName = 'golf-sim-v2'; // Incremented version
+const cacheName = 'golf-sim-v2'; // Change this to v3, v4, etc. to force an update
 const assets = [
   'index.html',
   'manifest.json',
-  'icon-jmm-1024.png' // Added your icon to the cache list
+  'icon-png.png'
 ];
 
+// 1. INSTALL: Save the files into the "Vault" (Cache)
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
-      // We add each asset individually to prevent one failure from breaking the whole cache
       return Promise.all(
         assets.map(url => cache.add(url).catch(err => console.log('Failed to cache:', url, err)))
       );
@@ -16,6 +16,23 @@ self.addEventListener('install', e => {
   );
 });
 
+// 2. ACTIVATE: Housekeeping - Delete old versions of the cache
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== cacheName) {
+            console.log('Cleaning up old cache:', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 3. FETCH: Serve from the "Vault" if offline
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(res => {
